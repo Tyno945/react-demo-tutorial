360dd220ef76bf634c561096425f985f94c06246
he.fang <he.fangh@01bees.com>
2019-05-05 20:17:48 +0800

    Updates

---
+++ precache-manifest.f74d18eb4322d0c284ac7ecd5dcc7089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca7c8253602ba573c40ba78097eca41",
+    "url": "/react-demo-tutorial/index.html"
+  },
+  {
+    "revision": "cde4ec7c06f91dab1b30",
+    "url": "/react-demo-tutorial/static/css/main.11f5f98f.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-demo-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "cde4ec7c06f91dab1b30",
+    "url": "/react-demo-tutorial/static/js/main.81c72530.chunk.js"
+  },
+  {
+    "revision": "f334e61888d847ae7573",
+    "url": "/react-demo-tutorial/static/js/runtime~main.b2cd285c.js"
+  }
+]);
